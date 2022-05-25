3d2ab89f144a1f8adeeb48eb9b932bd8748f0413
Cory Popp <cpopp9@gmail.com>
2022-05-25 14:59:24 -0400

    Initial Commit

---
+++ MovieNight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieNight
+//
+//  Created by Cory Popp on 5/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieNight/MovieNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieNightApp.swift
+//  MovieNight
+//
+//  Created by Cory Popp on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
