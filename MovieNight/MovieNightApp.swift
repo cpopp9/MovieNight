122d4a51497d8645b81c3afb563b8418405b2b29
Cory Popp <cpopp9@gmail.com>
2022-09-29 11:44:48 -0400

    Initial Commit

---
+++ MovieNight/MovieNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieNightApp.swift
+//  MovieNight
+//
+//  Created by Cory Popp on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
