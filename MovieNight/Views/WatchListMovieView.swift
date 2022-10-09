e3e8ccb003449ab85e5797e29f80eb545e44fc0b
Cory Popp <cpopp9@gmail.com>
2022-10-09 14:04:12 -0400

    Removed extraneous code

--- MovieNight/Views/WatchListMovieView.swift
+++ MovieNight/Views/WatchListMovieView.swift
@@ -72,31 +72,6 @@ struct WatchListMovieView: View {
         }
         .ignoresSafeArea()
         
-        
-            //        List {
-            //            AddToWatchListButton(movie: movie)
-            //
-            //            Section("Movie Details") {
-            //                Text("title: \(movie.wrappedTitle)")
-            //                Text("id: \(String(movie.id))")
-            //                Text("media_type: \(movie.wrappedMediaType)")
-            //                Text("original_title: \(movie.wrappedOriginalTitle)")
-            //                Text("original_language: \(movie.wrappedOriginalLanguage)")
-            //                Text("overview: \(movie.wrappedOverview)")
-            //                Text("Release Date: \(movie.wrappedReleaseDate)")
-            //            }
-            //
-            //            Section("Media Images") {
-            //                Text("backdrop_path: \(movie.wrappedBackdropPath)")
-            //                Text("poster_path: \(movie.wrappedPosterPath)")
-            //            }
-            //
-            //            Section("Voting") {
-            //                Text("vote_average: \(String(movie.vote_average))")
-            //                Text("vote_count: \(String(movie.vote_count))")
-            //            }
-            //        }
-            //        .navigationTitle(movie.wrappedTitle)
     }
 }
 
