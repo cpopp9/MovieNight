3cc62aafcd243bbd314d258833dc6b82636f54d7
Cory Popp <cpopp9@gmail.com>
2022-10-12 11:58:30 -0400

    optimized button layout

--- MovieNight/Views/Search/SearchAddToWatchlistButton.swift
+++ MovieNight/Views/Search/SearchAddToWatchlistButton.swift
@@ -1,9 +1,9 @@
-//
-//  SearchAddToWatchlistButton.swift
-//  MovieNight
-//
-//  Created by Cory Popp on 10/11/22.
-//
+    //
+    //  SearchAddToWatchlistButton.swift
+    //  MovieNight
+    //
+    //  Created by Cory Popp on 10/11/22.
+    //
 
 import CoreData
 import SwiftUI
@@ -14,26 +14,46 @@ struct SearchAddToWatchlistButton: View {
     @State var watchlistObject: WatchlistMedia?
     
     var body: some View {
-        
-        Button() {
-            
-            (watchlistObject != nil) ? deleteFromWatchlist() : saveToWatchlist()
-            
-        } label: {
-            HStack {
-                Image(systemName: (watchlistObject != nil) ? "minus" : "plus")
-                Text((watchlistObject != nil) ? "Remove from Watchlist" : "Add to watchlist")
+        VStack {
+            Button() {
+                
+                (watchlistObject != nil) ? deleteFromWatchlist() : saveToWatchlist()
+                
+            } label: {
+                HStack {
+                    Image(systemName: (watchlistObject != nil) ? "minus" : "plus")
+                    Text((watchlistObject != nil) ? "Remove from Watchlist" : "Add to watchlist")
+                }
+                .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
-        }
-        .tint((watchlistObject != nil) ? .red : .blue)
-        .buttonStyle(.borderedProminent)
-        .buttonBorderShape(.roundedRectangle(radius: 10))
-        .controlSize(.large)
-        .padding()
-        
-        .task {
+            .tint((watchlistObject != nil) ? .red : .blue)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 10))
+            .controlSize(.large)
+            
+            Button() {
+//                loadObject(contentId: Int(media.id))
+                    watchlistObject?.watched.toggle()
                 loadObject(contentId: Int(media.id))
+            } label: {
+                HStack {
+                    if let watchlistObject = watchlistObject {
+                        Image(systemName: watchlistObject.watched ? "checkmark.circle.fill" : "checkmark.circle")
+                    } else {
+                        Image(systemName: "checkmark.circle")
+                    }
+                    Text("Watched")
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .tint((watchlistObject?.watched != nil) ? .gray : .green)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 10))
+            .controlSize(.large)
+            .disabled(watchlistObject == nil)
+        }
+        .task {
+            loadObject(contentId: Int(media.id))
         }
     }
     
@@ -48,20 +68,20 @@ struct SearchAddToWatchlistButton: View {
     }
     
     func saveToWatchlist() {
-            let newItem = WatchlistMedia(context: moc)
-            newItem.title = media.title ?? "Unknown"
-            newItem.id = Int32(media.id)
-            newItem.backdrop_path = media.backdrop_path
-            newItem.poster_path = media.poster_path
-            newItem.media_type = media.media_type
-            newItem.original_language = media.original_language
-            newItem.original_title = media.original_title
-            newItem.overview = media.overview
-            newItem.vote_average = media.vote_average
-            newItem.vote_count = media.vote_count
-            newItem.watched = false
-            
-            try? moc.save()
+        let newItem = WatchlistMedia(context: moc)
+        newItem.title = media.title ?? "Unknown"
+        newItem.id = Int32(media.id)
+        newItem.backdrop_path = media.backdrop_path
+        newItem.poster_path = media.poster_path
+        newItem.media_type = media.media_type
+        newItem.original_language = media.original_language
+        newItem.original_title = media.original_title
+        newItem.overview = media.overview
+        newItem.vote_average = media.vote_average
+        newItem.vote_count = media.vote_count
+        newItem.watched = false
+        
+        try? moc.save()
         
         watchlistObject = newItem
     }
@@ -75,11 +95,25 @@ struct SearchAddToWatchlistButton: View {
         
         try? moc.save()
     }
+    
+    func markAsWatched() {
+        if let watchlistObject = watchlistObject {
+            watchlistObject.watched = true
+            
+                //            loadObject(contentId: Int(media.id))
+        }
+    }
+    
+    func markAsNotWatched() {
+        if let watchlistObject = watchlistObject {
+            watchlistObject.watched = false
+        }
+    }
 }
-//
-//    struct SearchAddToWatchlistButton_Previews: PreviewProvider {
-//        static var previews: some View {
-//            AddToWatchListButton()
-//        }
-//    }
+    //
+    //    struct SearchAddToWatchlistButton_Previews: PreviewProvider {
+    //        static var previews: some View {
+    //            AddToWatchListButton()
+    //        }
+    //    }
 
