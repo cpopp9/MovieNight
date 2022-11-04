60592ca202808de8d0f412c84f8c3d694e1e3dee
cpopp9 <104847711+cpopp9@users.noreply.github.com>
2022-11-04 12:03:58 -0400

    Optimized credits ui

--- MovieNight/Views/MovieView/CreditProfilePictures.swift
+++ MovieNight/Views/MovieView/CreditProfilePictures.swift
@@ -13,12 +13,11 @@ struct CreditProfilePictures: View {
         NavigationLink {
             PersonView(person: person)
         } label: {
-            ZStack {
+            VStack {
                 AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w342\(person.wrappedProfilePath)")) { image in
                     image
                         .resizable()
                         .scaledToFit()
-//                        .frame(width: 100)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                     
                 } placeholder: {
@@ -27,17 +26,10 @@ struct CreditProfilePictures: View {
                         .scaledToFit()
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
-                VStack(alignment: .trailing) {
-                    Spacer()
                     Text(person.wrappedName)
                         .font(.caption)
-                        .foregroundColor(.secondary)
-                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
                         .padding(5)
-                        .background(Color.black)
-                        
-                        .
-                }
             }
             .frame(width: 100)
         }
