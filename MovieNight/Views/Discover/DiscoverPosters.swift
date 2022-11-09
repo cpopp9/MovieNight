9cd0b6703f8ba2808206fb8b52f085c0433af402
cpopp9 <104847711+cpopp9@users.noreply.github.com>
2022-11-09 16:01:32 -0500

    Fixed discover ui bug

--- MovieNight/Views/Discover/DiscoverPosters.swift
+++ MovieNight/Views/Discover/DiscoverPosters.swift
@@ -14,20 +14,17 @@ struct DiscoverPoster: View {
             MovieView(media: media)
         } label: {
             VStack {
-                
                 AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w342\(media.poster_path ?? "")")) { image in
                     image
                         .resizable()
-                        .scaledToFit()
-                        .frame(maxHeight: 300)
+                        .aspectRatio(2/3, contentMode: .fill)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                     
                 } placeholder: {
                     Image("poster_placeholder")
                         .resizable()
                         .scaledToFit()
-                        .frame(maxHeight: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
                 
                 Text(media.wrappedTitle)
