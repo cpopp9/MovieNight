e73647712cfbbcf6ae809a563e8363ef6fa0d7ce
cpopp9 <104847711+cpopp9@users.noreply.github.com>
2022-11-14 14:49:45 -0500

    VT

--- MovieNight/Views/Data/DataController.swift
+++ MovieNight/Views/Data/DataController.swift
@@ -20,6 +20,8 @@ class DataController: ObservableObject {
             }
             
             self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+            
+            ValueTransformer.setValueTransformer(UIImageTransformer(), forName: NSValueTransformerName("UIImageTransformer"))
         }
         
         deleteObjects(filter: .nonWatchlist)
